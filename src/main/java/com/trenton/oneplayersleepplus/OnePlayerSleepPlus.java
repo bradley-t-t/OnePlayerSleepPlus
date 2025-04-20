280389378916b05d97a67ae0d2083305818c0894
Bradley Taylor <trenton.taylor.email@gmail.com>
2025-04-20 12:09:05 -0500

    Initial commit.

---
+++ src/main/java/com/trenton/oneplayersleepplus/OnePlayerSleepPlus.java
@@ -0,0 +1,17 @@
+package com.trenton.oneplayersleepplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OnePlayerSleepPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/trenton/oneplayersleepplus/listeners/SleepListener.java
@@ -0,0 +1,2 @@
+package com.trenton.oneplayersleepplus.listener;public class SleepListener {
+}
